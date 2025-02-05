853f468b2008dbb8b165a5b18c5f6d445d4e8778
Lukaka <monishine_code@163.com>
2025-02-05 14:35:34 +0800

    feat(config): base

--- astro.config.ts
+++ astro.config.ts
@@ -8,7 +8,7 @@ import mdx from "@astrojs/mdx";
 // https://astro.build/config
 export default defineConfig({
   site: THEME_CONFIG.website,
-  base: '/',
+  base: '/diary-blog',
   prefetch: true,
   markdown: {
     shikiConfig: {
